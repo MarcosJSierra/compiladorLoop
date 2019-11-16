d3c700316df6e85437716e43b43db60c409309d1
sharongomez98 <sharongomez98@hotmail.com>
2019-11-16 11:09:10 -0600

    cambios

--- compiladorLoop/src/main/java/informacion/Nodo.java
+++ compiladorLoop/src/main/java/informacion/Nodo.java
@@ -14,7 +14,7 @@ public class Nodo {
     public static final int TIPO_NUMERO=2;
     public static final int TIPO_OPERADOR=3;
     public static final int TIPO_SECCION=4;
-    
+     
     public static final int OP_SUMA = 10;
     public static final int OP_RESTA = 11;
     public static final int OP_MULTI = 12;
@@ -110,7 +110,7 @@ public class Nodo {
         {
             case SEC_INCLUDE:
             {
-                return "inlcuir";
+                return "incluir";
             }
             case SEC_CLASE:
             {
